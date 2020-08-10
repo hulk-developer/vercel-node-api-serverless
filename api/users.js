d98b2972f63abbbc5c12162c2cb7fb9ac4bff7d7
Jane <jslvtr98@gmail.com>
2020-08-10 10:44:18 +0530

    Testing

--- api/users.js
+++ api/users.js
@@ -1,5 +1,33 @@
-module.exports = (req, res) => {
-  res.json({
-    message: "Hello World, This is Natalie Portman",
-  });
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const userModel = mongoose.model("User", UserSchema);
+
+module.exports = async (req, res) => {
+  try {
+    const user = new userModel({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    await user.save();
+
+    res.json({
+      message: `Successfully saved ${user.username}`,
+    });
+  } catch (e) {
+    res.status(400).json({
+      error: `Error: ${e.message}`,
+    });
+  }
 };
